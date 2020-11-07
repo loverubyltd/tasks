fa663b6be671cb668dbec9a37f9c538aa9857f8c
Alexander Brandon Coles <alex@alexbcoles.com>
2020-11-07 06:16:35 +0000

    prefs to do with auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -179,7 +179,7 @@ dependencies {
     implementation("androidx.annotation:annotation:1.1.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("androidx.preference:preference:1.1.1")
+    implementation("androidx.preference:preference-ktx:1.1.1")
     implementation("com.jakewharton.timber:timber:4.7.1")
     implementation("com.google.android.apps.dashclock:dashclock-api:2.0.0")
     implementation("com.twofortyfouram:android-plugin-api-for-locale:1.0.2") {
