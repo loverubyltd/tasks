856905ac6628e4ea079d5458d47f054bcb160fc9
Alexander Brandon Coles <alex@alexbcoles.com>
2020-11-16 13:23:04 +0000

    Set up Spotless and ktlint for code formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 buildscript {
     repositories {
         jcenter()
@@ -10,6 +12,40 @@ buildscript {
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.3.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}")
+    }
+}
+
+
+plugins {
+    id("com.diffplug.spotless") version "5.7.0"
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven(url = "https://jitpack.io")
+    }
+
+    apply(plugin = "com.diffplug.spotless")
+
+    configure<SpotlessExtension> {
+        ratchetFrom = "origin/main" // only format files which have changed since origin/main
+
+        kotlin {
+            target(fileTree(".") {
+                include("**/*.kt")
+                exclude("**/.gradle/**")
+                exclude("**/build/**")
+            })
+            ktlint()
+        }
+        kotlinGradle {
+            target(
+                "*.gradle.kts", "**/*.gradle.kts"
+            )
+            ktlint()
+        }
     }
 }
 
